c94ef876374fba21a6f1c2f887cf1ccf323b9084
Vishal Mhatre <v.mhatre56@gmail.com>
2019-02-20 18:07:34 +0530

    lec 67 completed

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,9 +1,9 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectId} = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const dbName = "TodoApp";
 
-MongoClient.connect(`mongodb://localhost:27017/${dbName}`,(err,client)=>{
-    if(err){
+MongoClient.connect(`mongodb://localhost:27017/${dbName}`, (err, client) => {
+    if (err) {
         console.log("Unable to connect to MongoDB server!");
     }
     console.log("Connected to MongoDB server.");
@@ -20,12 +20,34 @@ MongoClient.connect(`mongodb://localhost:27017/${dbName}`,(err,client)=>{
     //     console.log("Unable to get todos. Error:",err);
     // })
 
-    db.collection("Users").find({name: 'Vishal Mhatre'}).toArray().then((users)=>{
-        console.log("Users:");
-        console.log(JSON.stringify(users,undefined,2));
-    },(err)=>{
-        console.log("Unable to get users. Error:",err);
+    // db.collection("Users").find({name: 'Vishal Mhatre'}).toArray().then((users)=>{
+    //     console.log("Users:");
+    //     console.log(JSON.stringify(users,undefined,2));
+    // },(err)=>{
+    //     console.log("Unable to get users. Error:",err);
+    // })
+
+    // deleteMany
+    // db.collection("Todos").deleteMany({text: 'commit code'}).then((result)=>{
+    //     console.log(result);
+    // })
+    // db.collection("Users").deleteMany({name: 'Vishal Mhatre'}).then((res)=>{
+    //     console.log(res);
+    // })
+
+    //deleteOne
+    // db.collection("Todos").deleteOne({text: 'commit code'}).then((result)=>{
+    //     console.log(result);
+    // })
+
+    //findOneAndDelete
+    // db.collection("Todos").findOneAndDelete({ text: 'commit code' }).then((result) => {
+    //     console.log(result);
+    // })
+    db.collection("Users").findOneAndDelete({_id: new ObjectId('5c6d40d1ad69017e97c2088d')}).then((res)=>{
+        console.log(res);
     })
+
 
     // client.close();
 });
