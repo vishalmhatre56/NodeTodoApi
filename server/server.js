b25bbd965da017e6f880c4a0b80249d63a487cfd
Vishal Mhatre <v.mhatre56@gmail.com>
2019-02-21 17:38:04 +0530

    lec 84 completed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = reruire('lodash');
 
 const port = process.env.PORT || 3000;
 const { mongoose } = require('./db/mongoose');
@@ -23,7 +24,7 @@ app.post('/todos', (req, res) => {
 });
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (err) => {
         res.status(400).send(err);
     });
@@ -31,29 +32,54 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
-    if(!ObjectId.isValid(id))
-       return res.status(400).send("Id is not valid!");
+    if (!ObjectId.isValid(id))
+        return res.status(400).send("Id is not valid!");
 
     Todo.findById(id).then((todo) => {
-        if(todo)
-            return res.send({todo});
+        if (todo)
+            return res.send({ todo });
         return res.status(404).send('No todo found!')
-    }).catch((err)=>{
-       return res.status(400).send(err);
+    }).catch((err) => {
+        return res.status(400).send(err);
     });
 });
 
 app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
-    if(!ObjectId.isValid(id))
-       return res.status(400).send("Id is not valid!");
-       
+    if (!ObjectId.isValid(id))
+        return res.status(400).send("Id is not valid!");
+
     Todo.findByIdAndRemove(id).then((todo) => {
-        if(todo)
-            return res.send({todo});
+        if (todo)
+            return res.send({ todo });
         return res.status(404).send('No todo found!')
-    }).catch((err)=>{
-       return res.status(400).send(err);
+    }).catch((err) => {
+        return res.status(400).send(err);
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectId.isValid(id))
+        return res.status(400).send("Id is not valid!");
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, { new: true }).then((todo) => {
+        if (todo)
+            return res.send({ todo });
+        return res.status(404).send('No todo found!')
+    }).catch((err) => {
+        return res.status(400).send(err);
     });
 });
 
