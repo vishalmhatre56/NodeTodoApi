c8901a930bf85c348d9e30ff0e232272f18d025b
Vishal Mhatre <v.mhatre56@gmail.com>
2019-02-21 15:23:17 +0530

    lec 80 deploy on heroku with remote mongodb atlas

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user')
