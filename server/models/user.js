d4e80d9e8ad1bff259a671488b9c76bc5c5ae25c
Vishal Mhatre <v.mhatre56@gmail.com>
2019-02-25 17:25:48 +0530

    lec 95 completed

--- server/models/user.js
+++ server/models/user.js
@@ -69,16 +69,36 @@ UserSchema.statics.findByToken = function (token) {
     })
 }
 
-UserSchema.pre('save', function(next){
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password,user.password,(err, res)=>{
+                if(res){
+                    resolve(user);
+                }else{
+                    reject();
+                }
+            })
+        })
+    });
+};
+
+UserSchema.pre('save', function (next) {
     var user = this;
-    if(user.isModified('password')){
-        bcrypt.genSalt(10,(err, salt)=>{
-            bcrypt.hash(user.password,salt,(err, res)=>{
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, res) => {
                 user.password = res;
                 next();
             })
         })
-    }else{
+    } else {
         next();
     }
 })
