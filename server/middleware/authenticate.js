8c10295c0022311b18c9ae32bf6f4c9cae02e203
Vishal Mhatre <v.mhatre56@gmail.com>
2019-02-26 17:37:59 +0530

    bug fixed

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -5,12 +5,10 @@ var authenticate = (req, res, next) => {
 
     User.findByToken(token).then((user) => {
         if (!user) {
-            console.log(user)
-            return Promise.reject;
+            return Promise.reject();
         }
         req.user = user;
         req.token = token;
-        console.log("req:",req)
         next();
     }).catch((e) => {
         res.status(401).send();
